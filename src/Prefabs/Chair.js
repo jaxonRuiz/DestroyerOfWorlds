fca1aa1c75cea3e3e8c32428501ef40b0cb4a542
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-03-08 14:59:53 -0800

    general start on player and chair projectile
    nothing that really works, just messing around with things for now, will
    look at it later maybe

---
+++ src/Prefabs/Chair.js
@@ -0,0 +1,103 @@
+// chair ammo setup
+
+class AmmoManager {
+    constructor(scene, player, enemies) {
+        this.ammo = 0; // ammo count
+        // also see if we want to make a max ammo count
+        // see if we want to make the text visual
+        // this.ammoHUD = scene.add.text(x, y, `ammo text: ${this.ammo}`);
+        
+        // "pools"
+        this.collectableChairs = [];
+        
+        // save for later
+        this.scene = scene;
+    }
+
+    getAmmo() {
+        // ammo obtained SFX
+
+        this.ammo++;
+    }
+
+    shoot() {
+        this.ammo--;
+
+        // update HUD
+    }
+
+    deleteChair(chair) { // we won't ever have too many chairs on the screen hopefully
+        chair.destroy();
+        this.collectableChairs.splice(this.collectableChairs.indexOf(chair), 1);
+    }
+
+    update() {
+        // spawn ammo every so often
+        // update pools
+        for (const chair of this.collectableChairs) {
+            chair.update();
+        }
+    }
+}
+
+// chairs to pick up
+class Chair extends Phaser.GameObjects.Sprite {
+    constructor(player, manager) {
+        this.manager = manager; // object manager
+
+        manager.scene.physics.add.collider(player, this, () => {
+            manager.getAmmo();
+            manager.deleteChair(this);
+        });
+    }
+
+    update() {
+        // scroll
+        this.x -= game.settings.scrollSpeed;
+
+        // delete if offscreen
+        if (this.x + this.size < 0) {
+            this.manager.deleteChair(this);
+        }
+    }
+}
+
+// chairs to shoot
+class ChairBullet extends Phaser.GameObjects.Sprite {
+    constructor(player, enemies, manager) {
+        this.manager = manager; // object manager
+
+        // dirties
+        this.shot = false;
+
+        // for the math
+        this.speed = 4; // in pixels
+        this.angle = 0;
+
+        manager.scene.physics.add.collider(player, this, () => {
+            manager.getAmmo();
+            manager.deleteChair(this);
+        });
+    }
+
+    shoot() {
+        this.shot = true;
+        
+        // add object to scene
+        this.manager.scene.physics.add.existing(this);
+        this.body.setImmoveable(true);
+        this.manager.scene.add.existing(this);
+    }
+
+    update() {
+        if (this.shot) {
+            // update position using phsyics wooo
+            // remember to rotate the sprite
+
+            // delete when offscreen
+            if (this.x + this.size < 0) {
+                this.manager.deleteChair(this);
+            }
+        }
+    }
+}
--- src/Prefabs/Player.js
+++ src/Prefabs/Player.js
@@ -1,4 +1,20 @@
-class Player extends Phaser.GameObjects.Sprite {
+// player group
+class Player {
+    // planning state machine
+    constructor(scene) {
+        this.x = 50;
+        this.y = game.config.width/2;
+        this.mordecai = scene.physics.add.sprite(x, y, "rigby_S").setOrigin(0.5, 1);
+        this.rigby = scene.physics.add.sprite(x, this.mordecai.height, "rigby_S").setOrigin(0.5, 1);
+        this.hitbox; // extra hitbox for things on ground
+    }
 
+    update() {
+        // player movement
+    }
+
+    // hitting crater
+    // might need to create a separate border box just for the crater :(
+    hit() {
+    }
 }
-
