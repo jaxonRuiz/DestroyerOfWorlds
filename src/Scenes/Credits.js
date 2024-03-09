e2ed42cf699afb0cf5ba9b1a49bd6b0dbf67846a
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-03-09 02:50:03 -0800

    nightly updte

--- src/Scenes/Credits.js
+++ src/Scenes/Credits.js
@@ -4,6 +4,32 @@ class Credits extends Phaser.Scene {
         super("creditsScene");
     }
 
+    create() {
+        let creditsConfig = {
+            fontFamily: 'Comic Sans',
+            fontSize: "52px",
+            backgroundColor: "#ABCDEF",
+            align: "center",
+            color: "black"
+        }
+        // add sfx
+        this.click = this.sound.add("pewSFX");
+
+        // add text
+        this.add.text(game.config.width/3, game.config.height/3, "(temporary) credits scene", creditsConfig);
+        creditsConfig.fontSize = "30px";
+        this.add.text(game.config.width/2, game.config.height/2, "Enter to start | Esc for menu", creditsConfig);
+    }
     // sounds from pixabay
     // music 
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyEnter)) {
+            this.click.play();
+            this.scene.start("bossfightScene");
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyEsc)) {
+            this.click.play();
+            this.scene.start("menuScene");
+        }
+    }
 }
--- src/Scenes/GameOver.js
+++ src/Scenes/GameOver.js
@@ -2,4 +2,27 @@ class GameOver extends Phaser.Scene {
     constructor() {
         super("gameoverScene");
     }
+
+    create() {
+        let textConfig = {
+            fontFamily: 'Comic Sans',
+            fontSize: "52px",
+            backgroundColor: "#FF0000",
+            align: "center"
+        }
+
+        this.add.text(game.config.width/3, game.config.height/3, "(temporary) credits scene", textConfig);
+        textConfig.fontSize = "30px";
+        this.add.text(game.config.width/2, game.config.height/2, "Enter to start | Esc for menu", textConfig);
+    }
+    // sounds from pixabay
+    // music 
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyEnter)) {
+            this.scene.start("bossfightScene");
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyEsc)) {
+            this.scene.start("menuScene");
+        }
+    }
 }
