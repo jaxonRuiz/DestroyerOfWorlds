29ae654159bd0f85dce4bea658bbbd38a8c15042
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-03-08 22:22:01 -0800

    player rescale

--- src/Prefabs/Player.js
+++ src/Prefabs/Player.js
@@ -4,29 +4,29 @@ class Player {
     constructor(scene) {
         this.x = 150;
         this.y = game.config.width/2;
-        this.rigby = scene.add.sprite(this.x, this.y-250, "rigby_S")
+        this.rigby = scene.add.sprite(this.x, this.y-125, "rigby_S")
             .setOrigin(0.5, 1)
-            .setScale(0.75);
+            .setScale(0.35);
         this.mordecai = scene.add.sprite(this.x, this.y, "mordecai_S")
             .setOrigin(0.5, 1)
-            .setScale(0.75);
+            .setScale(0.35);
 
         // editing hitbox
-        this.hitbox = scene.physics.add.sprite(this.x, this.y, "empty").setOrigin(0.5, 1);
+        this.hitbox = scene.physics.add.sprite(this.x, this.y, "empty").setOrigin(0.5);
         // edit the bounding boxes later
-        this.hitbox.body.setSize(80, 40);
-        this.hitbox.body.setOffset(-20, -15);
+        this.hitbox.body.setSize(50, 20);
+        this.hitbox.body.setOffset(-15, -10);
 
         // state machines
         scene.rigbyFSM = new StateMachine('idle', {
             idle: new R_IdleState(),
             aim: new R_AimState(),
             shoot: new R_ShootState(),
-        }, [scene, this.rigby]);
+        }, [scene, {rigby: this.rigby, mordecai: this.mordecai}]);
         scene.mordecaiFSM = new StateMachine('run', {
             run: new M_RunState(),
             dash: new M_DashState(),
-        }, [scene, this.mordecai]);
+        }, [scene, {rigby: this.rigby, mordecai: this.mordecai}]);
     }
 
     update() {
@@ -85,7 +85,7 @@ class M_DashState extends State {
     enter(scene, player) {
         console.log("mordecai dashing");
         // run animation
-        player.on('animationcomplete', () => {
+        player.mordecai.on('animationcomplete', () => {
             this.stateMachine.transition('run');
         });
     }
